871ac2a1941828fa827a602b7177711c5095cfce
Scott Winiarski <Scott1w1@hotmail.com>
2023-10-04 11:35:50 -0500

    Restructured card layout

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MenuItem from './MenuItem';
 import Card from 'react-bootstrap/Card'
 import Badge from 'react-bootstrap/Badge';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const Course = (props) => {
     const {name, items, addToMenu} = props;
@@ -9,14 +11,16 @@ const Course = (props) => {
 
     return (
         <React.Fragment>
+            
             <h1>{name[0].toUpperCase() + name.substring(1) + plural}
             <Badge bg="secondary" text='light' pill className='ml-2'>{items.length} item{plural}</Badge>
             </h1>
-            
+            <Row>
             {items.map( i => 
                 <MenuItem key={Number(i.id) + 7} item={i} addToMenu={addToMenu} />
             )}
             <hr />
+            </Row>
         </React.Fragment>
     );
 }
