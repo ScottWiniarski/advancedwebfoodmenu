730051cbdfc791aa09aff0e40989c60c3443fb43
Scott Winiarski <Scott1w1@hotmail.com>
2023-10-04 11:46:24 -0500

    Beginning to create New Menu Item

---
+++ src/Components/NewMenuItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const NewMenuItem = () => {
+
+    return(
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+}
+
+export default NewMenuItem;
